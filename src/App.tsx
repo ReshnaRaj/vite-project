9f7cceeb17e52412e8753c56269c9f0c1b24d871
ReshnaRaj <reshnarajan84@gmail.com>
2024-01-09 16:26:44 +0530

    responsive login

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,25 @@ function Login() {
     <form className="bg-red-50 shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col md:flex-row">
       <img src='https://res.cloudinary.com/dsyln8j3g/image/upload/v1693994631/BImage_2023-01-15T03-44-44.317Z_image_ojknrz.jpg' className="w-full md:w-1/2 h-auto rounded-t-2xl md:rounded-2xl md:rounded-l-none" alt="Image"></img>
       <div className="w-full md:w-1/2 p-4">
-      <span className="font-family:'Dancing Script',cursive">Choose your footwear</span>
+      <div className="text-center">
+    <span className="font-DancingScript text-3xl text-red-600 ">Choose your footwear</span>
+  </div>
 
         {/* <!-- Your form content --> */}
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Username</label>
-          <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700" id="username" type="text" placeholder="Enter your username" />
+          <label className="block text-gray-700 text-base font-DancingScript  mb-2 transition-all duration-300 hover:py-3 hover:px-4">Username</label>
+          <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700 " id="username" type="text" placeholder="Enter your username" />
         </div>
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2"  >Contact No:</label>
+          <label className="block text-gray-700 text-base font-DancingScript mb-2 transition-all duration-300 hover:py-3 hover:px-4"  >Contact No:</label>
           <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700" id="phone" type="text" placeholder="Enter your contact no:" />
         </div>
         <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2"  >Password</label>
+          <label className="block text-gray-700 text-base font-DancingScript mb-2 transition-all duration-300 hover:py-3 hover:px-4"  >Password</label>
           <input className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700" id="password" type="password" placeholder="**********" />
         </div>
         <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-DancingScript py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
         </div>
       </div>
     </form>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        DancingScript:['Dancing Script', 'cursive']
+      },
+    },
   },
   plugins: [],
 }
